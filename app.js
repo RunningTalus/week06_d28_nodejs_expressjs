b151b30e577ad8fada3329846830a88fee88a4df
runningtalus <dave@davemay.org>
2014-05-07 10:08:43 -0600

    added Views, added Jade via npm, routed to multiple pages

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'jade');  //sets the view engine to jade
+app.set('views', __dirname + '/views');  //sets the full path with __dirname
 
+
+//route for home page
 app.get('/', function(req, res) {
-	res.send('<h1>Hello Boulder</h1>');
+	res.render('index');  //within express, this now refers to the index.jade- once we install jade via 'npm install jade --save'
+});
+
+//route for dodgeball page
+app.get('/dodgeball', function(req, res) {
+	res.render('dodgeball');
 });
 
 var server = app.listen(7414, function() {
